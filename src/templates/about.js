1ef892da154d944c1eeb53b04735b0d35cdb9258
Coleen Huang <coleenhuang@gmail.com>
2020-06-05 17:48:00 -0700

    finished about page template

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout/layout'
+import { RichText } from 'prismic-reactjs'
 
-export default () => {
-    return (<Layout>
-        <h2>
-            About
-        </h2>
-    </Layout>)
+export const query = graphql`
+query aboutQuery($uid: String! $lang: String!){
+    prismic {
+        about(uid: $uid, lang: $lang) {
+          _meta {
+            id
+            lang
+            uid
+          }
+          body
+          image
+          title
+        }
+      }
+}`
+
+export default ({data}) => {
+    const about = data.prismic.about
+    return (
+    <Layout>
+        <RichText render={about.title} />
+        <img 
+            src={about.image.url} 
+            alt={about.image.alt} 
+            style={{display:'block', margin:'0 auto'}}/>
+        <RichText render={about.body} />
+    </Layout>
+    )
 }

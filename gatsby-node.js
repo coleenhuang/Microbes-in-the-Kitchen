9156984f904a37e777dd0057fab376b94f42f17b
Coleen Huang <coleenhuang@gmail.com>
2020-06-07 13:00:42 -0700

    programmatically create recipes page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,21 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require('path')
+exports.createPages = async ({ graphql, actions, reporter }) => {
+    const { createPage } = actions
+    const lang = ['en-us', 'zh-tw']
+    
+    // Create pages for each markdown file.
+    const allRecipesTemplate = path.resolve(`src/templates/allRecipes.js`)
+    lang.forEach((language) => {
+        const prefix = language ==='zh-tw'?'/zh/':'/'
+        const path = `${prefix}recipes`
+        console.log(path)
+        createPage({
+            path,
+            component: allRecipesTemplate,
+            context: {
+                pagePath: path,
+                lang: language
+            }
+        })
+    })
+  }
---
+++ src/templates/allRecipes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout/layout'
+
+//export const query = graphql`
+//`
+
+export default () => {
+    return (
+        <Layout>
+            <h2>Recipes</h2>
+        </Layout>
+    )
+}

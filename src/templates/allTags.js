ec0375809e7a962ad66059f01e414ca904d08e5c
Coleen Huang <coleenhuang@gmail.com>
2020-06-07 15:40:02 -0700

    alltag page queries added

--- src/templates/allTags.js
+++ src/templates/allTags.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout/layout'
+import { node } from 'prop-types'
 
-export default () => {
+export const query = graphql`
+query alltagQuery($lang: String){
+    prismic {
+        allTags(lang: $lang, sortBy: tag_ASC) {
+          edges {
+            node {
+              tag
+              _meta {
+                id
+                uid
+              }
+            }
+          }
+        }
+    }
+}`
+export default ({data}) => {
+    const tags = data.prismic.allTags.edges
     return(
         <Layout>
             <h2>Tags</h2>
+            {tags.map(({ node }) => (
+                <p key={node._meta.id} style={{textAlign:'center'}}>{node.tag}</p>
+            ))}
         </Layout>
     )
 }

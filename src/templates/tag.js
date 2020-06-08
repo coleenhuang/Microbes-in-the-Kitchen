7cf50483c23272d248cd4294514a861f28666e70
Coleen Huang <coleenhuang@gmail.com>
2020-06-07 17:13:29 -0700

    fixed title bug on tag pages

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -4,19 +4,15 @@ import Layout from '../components/layout/layout';
 import Article from  '../components/Article'
 
 export const query = graphql`
-query tagQuery($uid: String $lang: String!){
+query tagQuery($uid: String! $lang: String!){
     prismic {
-        allTags(uid: $uid, lang: $lang) {
-          edges {
-            node {
-              _meta {
-                id
-                lang
-                uid
-              }
-              tag
-            }
-          }
+        tag(uid: $uid, lang: $lang) {
+          tag
+          _meta {
+            id
+            uid
+            lang
+          }          
         }
         allPosts(lang: $lang) {
             edges {
@@ -52,25 +48,22 @@ query tagQuery($uid: String $lang: String!){
 }`
 
 export default ({data}) => {
-  const tag = data.prismic.allTags.edges.slice(0,1).pop()
+  const tag = data.prismic.tag
   const posts = data.prismic.allPosts.edges.filter(
     ({node}) => {
       const t = node.body1.find((i) => i.type ==='tags')
-      console.log('t', t)
       const tagList =[]
       if(t===undefined){
         return false
       }
       t.fields.forEach(ta => tagList.push(ta.tag.tag))
-      console.log(tagList)
-      console.log(tag.node.tag)
-      return tagList.includes(tag.node.tag)
+      return tagList.includes(tag.tag)
     }
   )
     console.log('posts', posts)
   return (
     <Layout>
-        <h2 key={tag.node._meta.id}>{tag.node.group}</h2>
+        <h2 key={tag._meta.id}>{tag.tag}</h2>
         {posts.map(({ node }, index) => {
             return(
               <Article key={index} node={node} />

20fc282a7e2e53ac9841c23f1d9526c80c5fa012
Coleen Huang <coleenhuang@gmail.com>
2020-06-26 10:19:02 -0700

    added lang to link resolver

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,16 +1,16 @@
 module.exports = {
   linkResolver(doc) {
     if (doc.type === 'Recipe') {
-      return `/recipe/${doc.uid}`;
+      return `${doc.lang}/recipe/${doc.uid}`;
     }
     if (doc.type === 'Post') {
-      return `/post/${doc.uid}`;
+      return `${doc.lang}/post/${doc.uid}`;
     }
     if (doc.type === 'Group') {
-      return `/group/${doc.uid}`;
+      return `${doc.lang}/group/${doc.uid}`;
     }
     if (doc.type ==='Tag') {
-      return `/tag/${doc.uid}`
+      return `${doc.lang}/tag/${doc.uid}`
     }
     return '/'
   },

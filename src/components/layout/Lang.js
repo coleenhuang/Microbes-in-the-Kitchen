edc00154c5c9f39a8dd3dc0c3551cb51302e9b8c
Coleen Huang <coleenhuang@gmail.com>
2020-07-01 16:27:29 -0700

    lang change button working

--- src/components/layout/Lang.js
+++ src/components/layout/Lang.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTranslation, language } from 'react-i18next'
 
 const Lang = () => {
+  const { t, i18n } = useTranslation()
+  const [lang, setLang] = useState('en-us');
+
+  function changeLang() {
+    let lang = i18n.language==='en-us'?'zh-tw':'en-us'
+    i18n.changeLanguage(lang)
+    setLang(lang);
+  }
+
   return (
-    <div>中</div>
+    <div onClick={changeLang}>{t("langMenu")}</div>
   )
 }
 
